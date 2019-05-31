1cc205d06df6f883bf50a54e82929e642cc048e3
LongHT <38778007+longhuynhthanh@users.noreply.github.com>
2019-05-31 21:21:14 +0700

    upload source

---
+++ Version1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Version1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ptrVo = new System.Windows.Forms.PictureBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.lblThongTin = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ptrVo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptrVo
+            // 
+            this.ptrVo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ptrVo.BackgroundImage")));
+            this.ptrVo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptrVo.Location = new System.Drawing.Point(107, 25);
+            this.ptrVo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ptrVo.Name = "ptrVo";
+            this.ptrVo.Size = new System.Drawing.Size(700, 372);
+            this.ptrVo.TabIndex = 0;
+            this.ptrVo.TabStop = false;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimKiem.Location = new System.Drawing.Point(332, 428);
+            this.btnTimKiem.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(250, 73);
+            this.btnTimKiem.TabIndex = 1;
+            this.btnTimKiem.Text = "Tìm Kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // lblThongTin
+            // 
+            this.lblThongTin.AutoSize = true;
+            this.lblThongTin.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThongTin.Location = new System.Drawing.Point(829, 128);
+            this.lblThongTin.Name = "lblThongTin";
+            this.lblThongTin.Size = new System.Drawing.Size(0, 31);
+            this.lblThongTin.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1012, 588);
+            this.Controls.Add(this.lblThongTin);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.ptrVo);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Tìm Kiếm Vợ Tương Lai";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptrVo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptrVo;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Label lblThongTin;
+    }
+}
+
---
+++ Version1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Version1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] ListAnhVo;
+        string[,] ThongTin = {
+                               { "Ngoại Hình: Cười Đẹp", "Tính Tình: Tốt Bụng"},
+                               {"Ngoại Hình: Mắt Đẹp", "Tính Tình: Siêng Năng"},
+                               {"Ngoại Hình: Da Đẹp", "Tính Tình: Tốt Tính"},
+                               {"Ngoại Hình: Dáng Đẹp", "Tính Tình: Hiền Lành" },
+                               { "Ngoại Hình: Dáng Chuẩn", "Tính Tình: Kute" }
+                             };
+        int index;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(1200, 600);
+            ListAnhVo = new string[5];
+            for (int i = 1; i <= 5; i++)
+            {
+                ListAnhVo[i - 1] = "VoTuongLai/" + i + ".png";
+            }
+            ThemHinhVo();
+        }
+
+        private void ThemHinhVo()
+        {
+            Random rd = new Random();
+            index = rd.Next(0, ListAnhVo.Length);
+
+            ptrVo.Image = Image.FromFile(ListAnhVo[index]);
+            ptrVo.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            for (int i = 0; i < ThongTin.GetLength(1); i++)
+            {
+                lblThongTin.Text += "\n" + ThongTin[index, i];
+            }
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            lblThongTin.ResetText();
+            ThemHinhVo();
+        }
+    }
+}
